4d6a95536aed1ea3a28510a54a311b22d3a288d2
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 21:25:21 +0000

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows< ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |m|
+      m.string :name
+      m.string :network
+      m.string :day
+      m.integer :rating
+    end
+  end
+end
