0e09b4c106c6e3c6b4d76ac1e9e7b87a0eec308d
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 22:02:20 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -14,16 +14,17 @@ def least_popular_show
 end
 
 def ratings_sum
-  <class show>.sum(:<rating>)
+  #<class show>.sum(:<rating>)
 end
 
 def popular_shows
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  shows = Show.where("rating > ?", 5)
+  
+  #shows = Show.where("rating > ?", 5)
 end
 
 def shows_by_alphabetical_order
-  Show.order(name: :asc)
+  #Show.order(name: :asc)
 end
 end
