04e61082abe9c18a9a544d599ba2afe0ad3d9135
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 03:38:06 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,39 @@
 class Show < ActiveRecord::Base
   
-  def highest_rating
+  def create
+    show = Show.new 
+  end 
+    
   
-end 
+  def self.highest_rating
+    Show.maximum(:rating)
+  end 
+    
+  def self.most_popular_show
+    x = Show.highest_rating
+    shows = Show.where("rating >= ?", x)
+    shows[0]
+  end
 
-def most_popular_show
-end
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
 
-def lowest_rating
-end
+  def self.least_popular_show
+    x = Show.lowest_rating
+    shows = Show.where("rating <= ?", x)
+    shows[0]
+  end
 
-def least_popular_show
-end
-
-def ratings_sum
-  #<class show>.sum(:<rating>)
-end
-
-def popular_shows
-  # For this test return all movies released after 2002 and ordered by 
-  # release date descending
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
   
-  #shows = Show.where("rating > ?", 5)
-end
-
-def shows_by_alphabetical_order
-  #Show.order(name: :asc)
-end
+  def self.popular_shows
+    shows = Show.where("rating > ?", 5)
+  end
+  
+  def self.shows_by_alphabetical_order
+    Show.order(:name)
+  end
 end
