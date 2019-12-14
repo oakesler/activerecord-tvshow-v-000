6cf922c508b3289efc60697658627f7c2b8e17a8
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 03:10:12 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,7 +1,9 @@
 class AddSeasonToShows< ActiveRecord::Migration[5.2]
   def change
-    add_column :shows do |t|
+    add_column :shows, :season, :string
       t.string :season
+      
+      add_column :products, :part_number, :string
     end
   end
 end
